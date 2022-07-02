78e15e64538584e800f40ea0c7204ec578bd5579
mryusuf <muhamadyusufindra@gmail.com>
2022-07-02 19:25:45 +0700

    Initial Commit

---
+++ Jelajah/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Jelajah
+//
+//  Created by Indra Permana on 02/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
